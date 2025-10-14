e7789777983b56acaa0b61054f43476c86be72a2
Shivom76 <shivompaul76@gmail.com>
2025-10-15 00:22:25 +0530

    submit all

---
+++ nodeServer/index.js
@@ -0,0 +1,30 @@
+const io=require("socket.io")(8000,{
+    cors: {
+        // 1. Specify the exact origin the client is connecting from
+        //    (The address where your index.html is loaded)
+        origin: "http://127.0.0.1:5500", 
+        
+        // 2. Specify the methods you will use (GET, POST, etc.)
+        methods: ["GET", "POST"]
+    }
+})
+
+let users={}
+
+io.on("connection",(socket)=>{
+
+    socket.on("newUser",(name)=>{
+        users[socket.id]=name
+        socket.broadcast.emit('newUser',name)
+        console.log("New User",name)
+    })
+
+    socket.on('send',(message)=>{
+        socket.broadcast.emit('recieve',{message:message,name:users[socket.id]})
+    })
+
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("offline",{message:`went offline`,name:users[socket.id]})
+        console.log("Disconnected")
+    })
+})
